c608848f501ce74a7983b2371a0b24f557ce1827
TheEGodOfficial <112714914+TheEGodOfficial@users.noreply.github.com>
2023-05-30 21:06:08 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2,8 +2,8 @@ local hiddenfling = false
 
 game:GetService("UserInputService").InputBegan:Connect(function(Input, GPE)
 	if GPE then return end
-		
-	if Input.KeyCode == _G.PunchKeyCode then
+
+	if Input.KeyCode == Enum.KeyCode[getgenv().PunchKeyCode] then
 		local p = game:GetService("Players").LocalPlayer
 		local c = p.Character
 		local h = c:FindFirstChildOfClass("Humanoid")
@@ -33,27 +33,29 @@ end)
 
 local hrp, c, vel, movel = nil, nil, nil, 0.1
 
-while true do
-	game:GetService("RunService").Heartbeat:Wait()
-	if hiddenfling then
-		local lp = game.Players.LocalPlayer
-		while hiddenfling and not (c and c.Parent and hrp and hrp.Parent) do
-			game:GetService("RunService").Heartbeat:Wait()
-			c = lp.Character
-			hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
-			if hiddenfling then
-				vel = hrp.Velocity
-				hrp.Velocity = vel * 10000 + Vector3.new(0, 10000, 0)
-				game:GetService("RunService").RenderStepped:Wait()
-				if c and c.Parent and hrp and hrp.Parent then
-					hrp.Velocity = vel
-				end
-				game:GetService("RunService").Stepped:Wait()
-				if c and c.Parent and hrp and hrp.Parent then
-					hrp.Velocity = vel + Vector3.new(0, movel, 0)
-					movel = movel * -1
+task.spawn(function()
+	while true do
+		game:GetService("RunService").Heartbeat:Wait()
+		if hiddenfling then
+			local lp = game.Players.LocalPlayer
+			while hiddenfling and not (c and c.Parent and hrp and hrp.Parent) do
+				game:GetService("RunService").Heartbeat:Wait()
+				c = lp.Character
+				hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
+				if hiddenfling then
+					vel = hrp.Velocity
+					hrp.Velocity = vel * 10000 + Vector3.new(0, 10000, 0)
+					game:GetService("RunService").RenderStepped:Wait()
+					if c and c.Parent and hrp and hrp.Parent then
+						hrp.Velocity = vel
+					end
+					game:GetService("RunService").Stepped:Wait()
+					if c and c.Parent and hrp and hrp.Parent then
+						hrp.Velocity = vel + Vector3.new(0, movel, 0)
+						movel = movel * -1
+					end
 				end
 			end
 		end
 	end
-end
+end)
