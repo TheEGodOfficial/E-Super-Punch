15a745b30de4583fa2b521fd142c6d54c67e58eb
TheEGodOfficial <112714914+TheEGodOfficial@users.noreply.github.com>
2023-05-30 21:43:47 -0400

    Update Script.lua
    blah blah blah fixing stuff

--- Script.lua
+++ Script.lua
@@ -3,7 +3,7 @@ local hiddenfling = false
 game:GetService("UserInputService").InputBegan:Connect(function(Input, GPE)
 	if GPE then return end
 
-	if Input.KeyCode == Enum.KeyCode.K then
+	if Input.KeyCode == getgenv().PunchKeyCode then
 		local p = game:GetService("Players").LocalPlayer
 		local c = p.Character
 		local h = c:FindFirstChildOfClass("Humanoid")
@@ -31,9 +31,8 @@ game:GetService("UserInputService").InputBegan:Connect(function(Input, GPE)
 	end
 end)
 
-local hrp, c, vel, movel = nil, nil, nil, 0.1
-
-task.spawn(function()
+local function fling()
+	local hrp, c, vel, movel = nil, nil, nil, 0.1
 	while true do
 		game:GetService("RunService").Heartbeat:Wait()
 		if hiddenfling then
@@ -42,20 +41,22 @@ task.spawn(function()
 				game:GetService("RunService").Heartbeat:Wait()
 				c = lp.Character
 				hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
-				if hiddenfling then
-					vel = hrp.Velocity
-					hrp.Velocity = vel * 10000 + Vector3.new(0, 10000, 0)
-					game:GetService("RunService").RenderStepped:Wait()
-					if c and c.Parent and hrp and hrp.Parent then
-						hrp.Velocity = vel
-					end
-					game:GetService("RunService").Stepped:Wait()
-					if c and c.Parent and hrp and hrp.Parent then
-						hrp.Velocity = vel + Vector3.new(0, movel, 0)
-						movel = movel * -1
-					end
+			end
+			if hiddenfling then
+				vel = hrp.Velocity
+				hrp.Velocity = vel * 10000 + Vector3.new(0, 10000, 0)
+				game:GetService("RunService").RenderStepped:Wait()
+				if c and c.Parent and hrp and hrp.Parent then
+					hrp.Velocity = vel
+				end
+				game:GetService("RunService").Stepped:Wait()
+				if c and c.Parent and hrp and hrp.Parent then
+					hrp.Velocity = vel + Vector3.new(0, movel, 0)
+					movel = movel * -1
 				end
 			end
 		end
 	end
-end)
+end
+		 
+fling()
